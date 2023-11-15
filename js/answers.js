3db74ec78ebbc1198c33b41fb1cd98f960346301
Alexey Orekhov <alexorekhov2004@gmail.com>
2023-11-15 22:59:58 +0300

    Add files via upload

--- js/answers.js
+++ js/answers.js
@@ -1,80 +1,62 @@
 let block1 = document.getElementById("block-1");
 let block2 = document.getElementById("block-2");
 
-document.getElementById("task-1-button").onclick =  function()
-{
+document.getElementById("task-1-button").onclick = function () {
     document.getElementById("task-1-answer").innerText = "Снеговик";
 }
-document.getElementById("task-2-button").onclick =  function()
-{
+document.getElementById("task-2-button").onclick = function () {
     document.getElementById("task-2-answer").innerText = "Утро";
 }
-document.getElementById("task-3-button").onclick =  function()
-{
+document.getElementById("task-3-button").onclick = function () {
     document.getElementById("task-3-answer").innerText = "День рождение";
 }
-document.getElementById("task-4-button").onclick =  function()
-{
+document.getElementById("task-4-button").onclick = function () {
     document.getElementById("task-4-answer").innerText = "логистика";
 }
-document.getElementById("task-5-button").onclick =  function()
-{
+document.getElementById("task-5-button").onclick = function () {
     document.getElementById("task-5-answer").innerText = "приёмка";
 }
-document.getElementById("task-6-button").onclick =  function()
-{
+document.getElementById("task-6-button").onclick = function () {
     document.getElementById("task-6-answer").innerText = "маркировка";
 }
 
-document.getElementById("task-7-button").onclick =  function()
-{
+document.getElementById("task-7-button").onclick = function () {
     document.getElementById("task-7-answer").innerText = "транспорт";
 }
 
-document.getElementById("task-8-button").onclick =  function()
-{
+document.getElementById("task-8-button").onclick = function () {
     document.getElementById("task-8-answer").innerText = "контейнер";
 }
-document.getElementById("task-9-button").onclick =  function()
-{
+document.getElementById("task-9-button").onclick = function () {
     document.getElementById("task-9-answer").innerText = "оптимизация";
 }
-document.getElementById("task-10-button").onclick =  function()
-{
+document.getElementById("task-10-button").onclick = function () {
     document.getElementById("task-10-answer").innerText = "управление";
 }
-document.getElementById("task-11-button").onclick =  function()
-{
+document.getElementById("task-11-button").onclick = function () {
     document.getElementById("task-11-answer").innerText = "контроль";
 }
-document.getElementById("task-12-button").onclick =  function()
-{
+document.getElementById("task-12-button").onclick = function () {
     document.getElementById("task-12-answer").innerText = "транспортная услуга";
 }
-document.getElementById("task-13-button").onclick =  function()
-{
+document.getElementById("task-13-button").onclick = function () {
     document.getElementById("task-13-answer").innerText = "упаковка";
 }
-document.getElementById("task-14-button").onclick =  function()
-{
+document.getElementById("task-14-button").onclick = function () {
     document.getElementById("task-14-answer").innerText = "провайдер";
 }
-document.getElementById("task-15-button").onclick =  function()
-{
+document.getElementById("task-15-button").onclick = function () {
     document.getElementById("task-15-answer").innerText = "аутсорсинг";
 }
-document.getElementById("task-16-button").onclick =  function()
-{
+document.getElementById("task-16-button").onclick = function () {
     document.getElementById("task-16-answer").innerText = "паллета";
 }
 
-function FirstPage()
-{
+function FirstPage() {
     block1.style.display = "inline";
     block2.style.display = "none";
 }
-function SecondPage()
-{
+function SecondPage() {
     block1.style.display = "none";
     block2.style.display = "inline";
 }
